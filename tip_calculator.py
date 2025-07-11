3838ee4bfc78ac228405f4d2fa31d3eab8e02cf5
Prayshe <pryhmibrahim@gmail.com>
2025-07-11 23:45:47 +0800

    better version

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,14 +1,14 @@
 #Tip Calculator
-name = input('what is your name? ')
-cost = int(input('What is the cost of your meal? '))
+name = input('What is your name? ')
+cost = float(input('What is the cost of your meal? '))
 tip = int(input('What percentage tip would you like to give? '))
 split = int(input('How many of you to split the bill? '))
 
-your_tip = (tip / 100 * cost)
-total_bill = (your_tip + cost)
-split_bill = (cost / split)
+your_tip = tip / 100 * cost
+total_bill = cost + your_tip
+split_bill = total_bill / split
 
-print(f'Welcome, {name}')
-print(f'You should leave a tip of: ${your_tip}')
-print(f'total bill is: ${total_bill}')
-print(f'Each one of you should pay: ${split_bill}')
+print(f'\nWelcome, {name}!')
+print(f'You should leave a tip of: ${your_tip:.2f}')
+print(f'Total bill is: ${total_bill:.2f}')
+print(f'Each one of you should pay: ${split_bill:.2f}')
