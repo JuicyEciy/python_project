6597d292893d043a74fb4cf486a0f92a366e2632
Prayshe <pryhmibrahim@gmail.com>
2025-07-12 00:06:44 +0800

    simple project

---
+++ Distance_converter.py
@@ -0,0 +1,5 @@
+kilometer = float(input('How far is it (km)? '))
+
+y = kilometer * 0.621371
+
+print ("it's",y,"miles")
---
+++ age_calculator.py
@@ -0,0 +1,7 @@
+birth_year = int(input("Birth year: "))
+
+current_year = 2025
+
+age = current_year - birth_year
+
+print ('you are', (age),' years old')
